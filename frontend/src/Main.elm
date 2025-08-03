f1caacb61810edbc5d8504905364911033430bd3
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2025-08-03 10:14:32 +0200

    Avoid error message on user/storage local db read

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -770,6 +770,7 @@ handleUrlChange route model =
                 reloadLatestVoterTask : ConcurrentTask String (Maybe Gov.Id)
                 reloadLatestVoterTask =
                     Storage.read { db = model.db, storeName = "app" } govIdDecoder { key = "lastVoter" }
+                        |> ConcurrentTask.onError (\_ -> ConcurrentTask.succeed Nothing)
 
                 govIdDecoder =
                     JD.string |> JD.map Gov.idFromBech32
@@ -777,6 +778,7 @@ handleUrlChange route model =
                 reloadLatestStorageConfigTask : ConcurrentTask String StorageConfig
                 reloadLatestStorageConfigTask =
                     Storage.read { db = model.db, storeName = "app" } Page.Preparation.storageConfigDecoder { key = "lastStorageConfig" }
+                        |> ConcurrentTask.onError (\_ -> ConcurrentTask.succeed <| Page.Preparation.initStorageConfig model.ipfsPreconfig)
 
                 ( newTaskPool, taskCmds ) =
                     ConcurrentTask.Extra.attemptEach { pool = model.taskPool, send = sendTask, onComplete = OnTaskComplete }
