b7811e439dc5f7205ecda6df580230febe845837
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2026-02-04 18:05:21 +0100

    Show custom message when missing ada to pay Tx fees
    Signed-off-by: Matthieu Pizenberg <matthieu.pizenberg@gmail.com>

--- frontend/src/Page/Cart.elm
+++ frontend/src/Page/Cart.elm
@@ -5,9 +5,10 @@ import Cardano.Address as Address exposing (Address, CredentialHash)
 import Cardano.Cip30 as Cip30
 import Cardano.CoinSelection as CoinSelection
 import Cardano.Gov as Gov exposing (ActionId, Anchor, CostModels, Id(..))
+import Cardano.MultiAsset as MultiAsset
 import Cardano.Script as Script
 import Cardano.Transaction as Transaction exposing (Transaction)
-import Cardano.TxIntent as TxIntent exposing (Fee(..), TxFinalized, TxIntent, VoteIntent)
+import Cardano.TxIntent as TxIntent exposing (Fee(..), TxFinalizationError(..), TxFinalized, TxIntent, VoteIntent)
 import Cardano.Uplc as Uplc
 import Cardano.Utils as Utils
 import Cardano.Utxo as Utxo exposing (Output)
@@ -718,7 +719,20 @@ buildTx costModels localStateUtxos walletAddress votersIntents =
             (AutoFee { paymentSource = feeSource })
             []
         |> Result.map (\txFinalized -> { keyNames = keyNames, txFinalized = txFinalized })
-        |> Result.mapError TxIntent.errorToString
+        |> Result.mapError customTxBuildingError
+
+
+customTxBuildingError : TxFinalizationError -> String
+customTxBuildingError error =
+    case error of
+        FailedToPerformCoinSelection CoinSelection.MaximumInputCountExceeded ->
+            "Your wallet has too many small inputs. An easy fix is doing a transfer to yourself of at least 2 ADA. This will create a new UTxO with at least 2 ADA in it."
+
+        FailedToPerformCoinSelection (CoinSelection.UTxOBalanceInsufficient _) ->
+            "You don’t have enough ada in your wallet to pay for the Tx fees. We have two solutions here!\n\n1. Simply send some ada to your wallet, then refresh the page.\n\n2. Or you can change the connected wallet to another one with enough ada to pay for the Tx fees. No worry, your votes will not change. In the signing page, you’ll simply need to connect and sign with both the DRep wallet and the wallet paying the fees."
+
+        _ ->
+            TxIntent.errorToString error
 
 
 
